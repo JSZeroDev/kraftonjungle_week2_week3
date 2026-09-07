4193b8de0d93bab06d5d7ac3ddf694610c15f17d
JSZeroDev <sam12057@gmail.com>
2026-09-07 20:49:22 +0900

    08_greedy_coin.py 문제 풀이 완료 closes #38

--- week3/1. basic/08_greedy_coin.py
+++ week3/1. basic/08_greedy_coin.py
@@ -46,7 +46,12 @@ def make_change_greedy(change, coins):
     # TODO: 각 동전에 대해 반복
     ## 현재 동전으로 거슬러줄 수 있는 개수 계산    
     ## 개수가 0보다 크면 결과에 추가
-    pass
+    for x in range(len(coins)) :
+        if change >= coins[x] : 
+            result[coins[x]] = change // coins[x]
+            change = change % coins[x]
+
+    total_coins = sum(result.values())
     
     return total_coins, result
 
